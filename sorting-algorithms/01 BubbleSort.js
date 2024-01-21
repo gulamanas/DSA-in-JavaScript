044ccaa89a6ad4c354882d0efd716afe47ae23ad
Gulam Anas <g.anas786@gmail.com>
2024-01-21 21:42:49 +0530

    bubble sort

---
+++ sorting-algorithms/01 BubbleSort.js
@@ -0,0 +1,23 @@
+function bubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+}
+
+console.log(bubbleSort([8, 20, -2, 4, -6]));
+
+// Time Complexity - O(n^2)
+
+// const arr = [8, 20, -2, 4, -6];
+// bubbleSort(arr);
+// console.log(arr);
