e597fad4b8b6eb9e5f09dc033e5707f101cb52ff
Gulam Anas <g.anas786@gmail.com>
2024-01-24 00:26:12 +0530

    quick sort

---
+++ sorting-algorithms/03 QuickSort.js
@@ -0,0 +1,26 @@
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  let sortedArray = [...quickSort(left), pivot, ...quickSort(right)];
+  return sortedArray;
+}
+
+// const arr = [8, 20, -2, 4, -6];
+// console.log(quickSort(arr));
+console.log(quickSort([8, 20, -2, 4, -6]));
+console.log(quickSort([8, 5, -2, -4, 100]));
+
+// Time complexity
+// Worst case = O(n^2)
+// Average case = O(nlogn)
