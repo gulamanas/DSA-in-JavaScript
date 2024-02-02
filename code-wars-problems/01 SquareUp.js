88b79697b319fa31c1943bb17ee752338e16a8e4
Gulam Anas <g.anas786@gmail.com>
2024-02-03 00:40:06 +0530

    square up

---
+++ code-wars-problems/01 SquareUp.js
@@ -0,0 +1,19 @@
+function squareUp(n) {
+  let result = [];
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n - i; j++) {
+      result.push(0);
+    }
+
+    for (let k = i; k >= 1; k--) {
+      result.push(k);
+    }
+  }
+
+  return result;
+}
+
+console.log(squareUp(3)); // Output: [0, 0, 1, 0, 2, 1, 3, 2, 1]
+console.log(squareUp(2)); // Output: [0, 1, 2, 1]
+console.log(squareUp(4)); // Output: [0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1]
