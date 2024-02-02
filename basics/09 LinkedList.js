12071cbf09660a1dfbc72a0f0554bf70f2fa5f0b
Gulam Anas <g.anas786@gmail.com>
2024-02-03 00:12:03 +0530

    prepend method

--- basics/09 LinkedList.js
+++ basics/09 LinkedList.js
@@ -18,8 +18,21 @@ class LinkedList {
   getSize() {
     return this.size;
   }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
 }
 
 const list = new LinkedList();
 console.log('List is Empty? ', list.isEmpty());
 console.log('List Size ', list.getSize());
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
